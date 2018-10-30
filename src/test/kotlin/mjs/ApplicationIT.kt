5ea091b676ee931b413d425cf12844091b334fa6
Michael Strasser <mstrass@thoughtworks.com>
2018-10-31 08:47:52 +1000

    Bare-bones application class

---
+++ src/test/kotlin/mjs/ApplicationIT.kt
@@ -0,0 +1,13 @@
+package mjs
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootTest
+@ActiveProfiles("test")
+class ApplicationIT {
+
+    @Test
+    fun contextLoads() { }
+}
