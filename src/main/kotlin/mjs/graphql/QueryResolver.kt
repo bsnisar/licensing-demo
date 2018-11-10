d04a7d8707a9bb5b30732239ad3d1290162bd761
Michael Strasser <mstrass@thoughtworks.com>
2018-11-11 08:44:10 +1000

    Implemented GraphQL mutations

--- src/main/kotlin/mjs/graphql/QueryResolver.kt
+++ src/main/kotlin/mjs/graphql/QueryResolver.kt
@@ -1,38 +1,59 @@
 package mjs.graphql
 
+import com.coxautodev.graphql.tools.GraphQLMutationResolver
 import com.coxautodev.graphql.tools.GraphQLQueryResolver
+import mjs.projection.ApplicantView
 import mjs.projection.ApplicationView
+import mjs.projection.DocumentView
 import mjs.projection.IApplicantRepository
 import mjs.projection.IApplicationRepository
 import mjs.projection.IDocumentRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
+import java.time.Instant
 import java.util.Optional
 import java.util.UUID
 
 @Component
-class QueryResolver(@Autowired val applicationRepository: IApplicationRepository,
-                    @Autowired val documentRepository: IDocumentRepository,
-                    @Autowired val applicantRepository: IApplicantRepository) : GraphQLQueryResolver {
+class QueryResolver(
+    @Autowired val applicationRepository: IApplicationRepository,
+    @Autowired val documentRepository: IDocumentRepository,
+    @Autowired val applicantRepository: IApplicantRepository
+) : GraphQLQueryResolver, GraphQLMutationResolver {
 
-    fun getApplicationById(id: String): ApplicationView? {
-        val applicationId = UUID.fromString(id)
+    fun getApplicationById(applicationId: UUID): ApplicationView? {
         val application = nullOpt(applicationRepository.findById(applicationId)) ?: return null
-        return addApplicant(addDocuments(application))
+        return loadApplicant(loadDocuments(application))
     }
 
     fun applications(): List<ApplicationView> {
         return applicationRepository.findAll()
-            .map { addDocuments(it) }
-            .map { addApplicant(it) }
+            .map { loadDocuments(it) }
+            .map { loadApplicant(it) }
     }
 
-    fun addDocuments(application: ApplicationView): ApplicationView {
+    fun createApplication(applicationId: UUID, type: String, createTimestamp: String): ApplicationView {
+        return applicationRepository.save(ApplicationView(applicationId, type, Instant.parse(createTimestamp)))
+    }
+
+    fun addDocument(applicationId: UUID, id: UUID, type: String, contents: String): ApplicationView? {
+        val application = nullOpt(applicationRepository.findById(applicationId)) ?: return null
+        application.documents = listOf(documentRepository.save(DocumentView(id, applicationId, type, contents)))
+        return application
+    }
+
+    fun setApplicant(applicationId: UUID, id: UUID, firstName: String, lastName: String, email: String): ApplicationView? {
+        val application = nullOpt(applicationRepository.findById(applicationId)) ?: return null
+        application.applicant = applicantRepository.save(ApplicantView(id, applicationId, firstName, lastName, email))
+        return application
+    }
+
+    fun loadDocuments(application: ApplicationView): ApplicationView {
         application.documents = documentRepository.findByApplicationId(application.id)
         return application
     }
 
-    fun addApplicant(application: ApplicationView): ApplicationView {
+    fun loadApplicant(application: ApplicationView): ApplicationView {
         application.applicant = applicantRepository.findByApplicationId(application.id)
         return application
     }
