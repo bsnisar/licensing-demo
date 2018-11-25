534766f3f7a0975827ca3607dc62f1ecc45bde75
Michael Strasser <mstrass@thoughtworks.com>
2018-11-25 22:03:19 +1000

    Workaround for Spring context load failure in integration test

--- src/test/kotlin/mjs/LicensingApplicationIT.kt
+++ src/test/kotlin/mjs/LicensingApplicationIT.kt
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 class LicensingApplicationIT {
 
     @Test
-    fun contextLoads() { }
+    fun contextLoads() {
+    }
 }
