7ee25f2449fa082ae6c8dbc1c39ebb3ec51ad2d3
Michael Strasser <mstrass@thoughtworks.com>
2018-12-05 07:27:20 +1000

    Tidied up POM

--- src/main/kotlin/mjs/LicensingApplication.kt
+++ src/main/kotlin/mjs/LicensingApplication.kt
@@ -1,16 +1,10 @@
 package mjs
 
-import org.modelmapper.ModelMapper
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class LicensingApplication {
-
-    @Bean
-    fun modelMapper(): ModelMapper = ModelMapper()
-}
+class LicensingApplication
 
 fun main(args: Array<String>) {
     runApplication<LicensingApplication>(*args)
